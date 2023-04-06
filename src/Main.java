565e3ef53e2db0156f319c3037706d9995375c57
Dante <duartedantedev@gmail.com>
2023-04-06 15:41:22 -0300

    Definindo ações para o filme

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Movie myMovie = new Movie();
+        myMovie.name = "O Poderoso Chefão";
+        myMovie.yearOfRelease = 1972;
+        myMovie.durationInMinutes = 175;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        myMovie.exibitMovieInfo();
+        myMovie.evaluateIt(8);
+        myMovie.evaluateIt(10);
+        myMovie.evaluateIt(5);
+        System.out.println(myMovie.sumOfScores);
+        System.out.println(myMovie.totalOfScores);
+        System.out.println(myMovie.getAverageScore());
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Movie {
+public class Movie {
+    String name;
+    int yearOfRelease;
+    int durationInMinutes;
+    double sumOfScores;
+    int totalOfScores;
+    boolean includedInPlan;
+
+    void exibitMovieInfo() {
+        System.out.println("Nome do filme: " + name);
+        System.out.println("Ano de lançamento: " + yearOfRelease);
+    }
+
+    void evaluateIt(double score) {
+        sumOfScores += score;
+        totalOfScores++;
+    }
+
+    double getAverageScore() {
+        return sumOfScores / totalOfScores;
+    }
 }
